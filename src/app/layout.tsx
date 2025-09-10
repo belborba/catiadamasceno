1dea6fa937261b9c41803d3df64af788220983d2
Bel Borba <isaborba.cs@gmail.com>
2025-09-10 19:09:20 -0300

    fix: final priority image test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,14 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const nunitoFont = localFont({
-  src: "/fonts/nunitosans.woff2",
+  src: "../fonts/nunitosans.woff2",
   variable: "--font-nunito",
   fallback: ["sans-serif"],
   display: "swap",
 });
 
 const parisFont = localFont({
-  src: "/fonts/paris2024.woff2",
+  src: "../fonts/paris2024.woff2",
   variable: "--font-title",
   fallback: ["sans-serif"],
   display: "swap",
